1f781c7b7ec102f62d725975b67ee4fadb97c99f
filipeotero <89042471+filipeotero@users.noreply.github.com>
2022-11-04 17:46:04 -0300

    more friendly splashscreen

--- src/Static.js
+++ src/Static.js
@@ -9,8 +9,6 @@ import { warningIcon, checkMarkIcon } from './encodedImages';
 
 import './Static.css';
 
-/*global chrome*/
-
 const importStatusEnum = {
   none: 1,
   error: 2,
@@ -28,11 +26,18 @@ class Static extends React.Component {
       importSettingsTitle: 'Import settings',
       errorDescription: 'Something went wrong when importing your custom setting file. Please try again or proceed with default settings.',
       signInTitle: this.props.signInTitle,
-      signInStatus: this.props.signInStatus
+      signInStatus: this.props.signInStatus,
+      loadingTime: 'Total loading time: '
     };
 
     window.setImportStatus = this.setImportStatus.bind(this);
+    window.setTotalLoadingTime = this.setTotalLoadingTime.bind(this);
   }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
   render() {
     return (
       <Container className='pr-3'>
@@ -43,8 +48,8 @@ class Static extends React.Component {
         </Row>
 
         <Row className='mt-3'>
-          <button id="btnSignIn" className='primaryButton' onClick={this.signIn}>
-          {this.state.signInTitle}
+          <button id='btnSignIn' className='primaryButton' onClick={this.signIn}>
+            {this.state.signInTitle}
           </button>
         </Row>
 
@@ -64,6 +69,7 @@ class Static extends React.Component {
               <input
                 type='file'
                 className='primaryButton'
+                accept='.xml'
                 onChange={(e) => this.readFile(e)}
               />
               <div className='buttonLabel'>
@@ -97,6 +103,11 @@ class Static extends React.Component {
             </span>
           </label>
         </Row>
+        <Row className='mt-3'>
+          <div className='p-0 loadingTimeFooter' >
+            {this.state.loadingTime}
+          </div>
+        </Row>
       </Container>
     );
   }
@@ -104,33 +115,34 @@ class Static extends React.Component {
   //Opens a page to signin
   //TODO: Localize strings by setting text as per what Dynamo sends in.
   signIn = async () => {
-    if (chrome.webview !== undefined ) {
-      if(this.state.signInStatus){
+    if (chrome.webview !== undefined) {
+      if (this.state.signInStatus) {
         var ret = await chrome.webview.hostObjects.scriptObject.SignOut();
-        this.setState({signInStatus: !ret,
-          signInTitle: "Sign In"})
+        this.setState({
+          signInStatus: !ret,
+          signInTitle: 'Sign In'
+        });
       }
-      else
-      {
-        var btn=document.getElementById("btnSignIn");
+      else {
+        var btn = document.getElementById('btnSignIn');
         btn.classList.add('disableButton');
         btn.disabled = true;
 
-        this.setState({signInTitle: "Signing In"})
-        var ret = await chrome.webview.hostObjects.scriptObject.SignIn();
-        this.setState({ signInStatus: ret });
+        this.setState({ signInTitle: 'Signing In' });
+        var ret2 = await chrome.webview.hostObjects.scriptObject.SignIn();
+        this.setState({ signInStatus: ret2 });
 
         btn.classList.remove('disableButton');
         btn.disabled = false;
-        if(ret){
-          this.setState({signInTitle: "Sign Out"})
+        if (ret) {
+          this.setState({ signInTitle: 'Sign Out' });
         }
-        else{
-          this.setState({signInTitle: "Sign In"})
+        else {
+          this.setState({ signInTitle: 'Sign In' });
         }
       }
     }
-  }
+  };
 
   //This method calls another method from Dynamo to actually launch it
   launchDynamo() {
@@ -164,10 +176,24 @@ class Static extends React.Component {
     });
   }
 
+
+  setTotalLoadingTime(loadingTime) {
+    this.setState({
+      loadingTime: loadingTime
+    });
+  }
+
   //Every time the checkbox is clicked, this method is called
   handleChange() {
     checked = !checked;
   }
+
+  handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      document.removeEventListener('keydown', this.handleKeyDown);
+      this.launchDynamo();
+    }
+  };
 }
 
 Static.defaultProps = {
@@ -179,7 +205,8 @@ Static.defaultProps = {
 Static.propTypes = {
   signInTitle: PropTypes.string,
   launchTitle: PropTypes.string,
-  showScreenAgainLabel: PropTypes.string
+  showScreenAgainLabel: PropTypes.string,
+  signInStatus: PropTypes.bool
 };
 
 export default Static;
